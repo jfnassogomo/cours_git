cadd8b423e41c9bf2f5ff24dc91fc53d99e83ba2
jf nassogomo <nassogomo.jf@gmail.com>
2020-04-03 11:58:13 +0200

    implémentation de la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,4 +1,30 @@
 
 public class Utilisateur {
+	/* __________props____________ */
+	private int id;
+	private String nom;
+	
+	/* ___________ ctors_____________ */
+	public Utilisateur() {
+		
+	}
 
+	/* ___________ setters/ getters_____________ */
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	
 }
